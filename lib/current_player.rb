13df728da327c048814c3f87a8f3674cb66aaeda
S.O <shakirat.odunsi@gmail.com>
2018-11-21 11:47:04 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,5 @@
-#turn_count
+
 def turn_count(board)
- # players =["X", "O"
  counter = []
    board.each do |turn|
      if turn == "X" || turn == "O"
@@ -10,13 +9,8 @@ def turn_count(board)
    counter.length.to_i
  end
 
-# puts "Up first is player #{turn} with #{counter.length.to_i}"
- # return counter
-
-  #current_player
 
   def current_player (board)
-#     turn_count
 if turn_count(board) % 2 == 0
 return "X"
 else
